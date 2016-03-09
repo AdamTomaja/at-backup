03b77f69292bf37ccb7fe5b9d549ff0f351da26c
cydercode_adam <cydercode@gmail.com>
2016-03-09 23:40:39 +0100

    Logging framework

--- src/main/java/pl/tomaja/App.java
+++ src/main/java/pl/tomaja/App.java
@@ -1,10 +1,24 @@
 package pl.tomaja;
 
-/**
- * Hello world!
- */
+import org.apache.commons.cli.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
 public class App {
+
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Options options = new Options();
+
+        options.addOption("s", true, "Source directory");
+        options.addOption("t", true, "Target directory");
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            LOGGER.error("Error during args parsing", e);
+        }
     }
 }
